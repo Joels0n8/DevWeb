f35ed4845977e70b4298e6725fa1cab63bcfa098
Joelson José de Macedo Carvalho <62727756+Joels0n8@users.noreply.github.com>
2021-11-18 19:27:01 -0300

    finalizando cases**

--- main.js
+++ main.js
@@ -9,26 +9,37 @@ function clean(){
 }
 
 function md5(){
-    var string = document.getElementById('string').innerHTML;
-    console.log(this.string.value);
+    var string = document.getElementById('string').textContent;
+    console.log(string);
 }
 
 function base64(){
-    
+    var string = document.getElementById('string').textContent;
 }
 
 function upper(){
-
-}
-
-function capital(){
-    
+    var string = document.getElementById('string').textContent;
+    document.getElementById('stringUpper').innerHTML = 'Upper Case: ' + string.toUpperCase();
 }
 
 function lower(){
-    
+    var string = document.getElementById('string').textContent;
+    document.getElementById('stringLower').innerHTML = 'Lower Case: ' + string.toLowerCase();
+}
+
+function capital(){
+    var string = document.getElementById('string').textContent;
+    document.getElementById('stringCapital').innerHTML = 'Capital Case: ' + string[0].toUpperCase() + string.substring(1);
 }
 
 function proper(){
+    var string = document.getElementById('string').textContent;
+    var arr = string.split(' ');
+    console.log(arr);
     
+    document.getElementById('stringProper').innerHTML = 'Proper Case: '
+    for(var i = 0; i < arr.length; i++){
+        var data = document.getElementById('stringProper').innerHTML;
+        document.getElementById('stringProper').innerHTML = data + ' ' + arr[i].substring(0,1).toUpperCase() + arr[i].substring(1);
+    }
 }
